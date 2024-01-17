45ff17b60d5e404ff7c38f434d5446fd279fb6dc
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-01-17 14:56:10 +0000

    [Start] 4-19

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,7 +2,7 @@ class PostCommentsController < ApplicationController
 
   def create
     post_image = PostImage.find(params[:post_image_id])
-    comment = current_user.post_comments.new(post_comment_params)
+    comment = PostComment.new(post_comment_params)
     comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image)
@@ -11,6 +11,7 @@ class PostCommentsController < ApplicationController
   def destroy
     PostComment.find(params[:id]).destroy
     redirect_to post_image_path(params[:post_image_id])
+  end
 
   private
 
